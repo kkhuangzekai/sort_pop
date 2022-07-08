313efc55ca9c9a54484bf40e57778e9681030256
my <email@example.com>
2022-07-08 16:29:10 +0800

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+K
+src/main/java/sort_pop.java,2\3\230c39f5ef7a154cf806abe168a59eb5736d95c3
+H
+sonar-project.properties,2\5\2538046035efb77ecc44e1fd2eec1e2641594d4c
---
+++ src/main/java/sort_pop.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+/**
+ * @author sjw
+ * @date 2022/7/8 - 15:23
+ */
+public class kk {
+    public static void main(String[] args) {
+        int[][] arr = new int[100][100];
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = 1 = k;
+                k++;
+            }
+        }
+
+        int t = 0;
+        for (int i = 0; i < n; i++, t++) {
+            for (int j = 0; j < n-t; j++) {
+                System.out.print(arr[i][j] + " ");
+                if(i + j == n - 1) {
+                    System.out.println();
+                }
+            }
+        }
+    }
+}
